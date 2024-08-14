6dc024db31427ae05f71f6d2251b1874bba13d8b
Luis Marquez <luis.marquez@e2ycommerce.com>
2024-08-14 18:30:35 +0200

    02 - Anatomía de una variable

---
+++ 02-anatomia-de-una-variable/clase-variables.js
@@ -0,0 +1,13 @@
+let cajaDeAndy = 'Woody';
+console.log(cajaDeAndy);
+
+// Lo NO Permitido
+// let c = 'Woody';
+// let cdAndy = 'Woody';
+// let pcdAndy = 'Woody';
+// let c, cda;
+
+// Lo Permitido
+// let primerTasteoDeAndy = 'Woody';
+// let urlDelUsuario = 'https://www.google.com';
+// let idDelUsuario = '1234567890';
