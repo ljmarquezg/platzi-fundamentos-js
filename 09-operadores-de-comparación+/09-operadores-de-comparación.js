d1b0b2f7ee1c1d282471946f8d112d120ead121e
Luis Marquez <luis.marquez@e2ycommerce.com>
2024-08-14 19:02:05 +0200

    09-operadores-de-comparación

---
+++ 09-operadores-de-comparación+/09-operadores-de-comparación.js
@@ -0,0 +1,18 @@
+const a = 10;
+const b = 20;
+const c = "10";
+
+a === b; // false
+a !== b; // true
+a === c; // false
+a !== c; // true
+a == c; // true
+a != c; // false
+a > b; // false
+a < b; // true
+a >= b; // false
+a <= b; // true
+a >= c; // true
+a <= c; // true
+a > c; // false
+a < c; // false
