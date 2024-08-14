a18091a1137c7b4b03f19a017e08dbcbcd2d50d8
Luis Marquez <luis.marquez@e2ycommerce.com>
2024-08-14 19:25:30 +0200

    15 - loop for of

---
+++ 15-loop-for-of/15-loop-for-of.js
@@ -0,0 +1,10 @@
+/* 
+for (variable of objeto) {
+    // Código a ejecutar
+}) */
+
+const canasta = ["manzana", "pera", "uva", "naranja", "sandía"];
+
+for (fruta of canasta) {
+    console.log(fruta);
+}
